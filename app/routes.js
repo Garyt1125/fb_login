63d572af9c0ff0eeaeabef38c6116c34a6df49c5
Soham Gaunekar <sgaunekar96@gmail.com>
2016-02-02 03:51:24 +0530

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,37 @@
+module.exports = function(app, passport) {
+
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user //
+        });
+    });
+
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
---
+++ config/auth.js
@@ -0,0 +1,8 @@
+ module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '', // your App ID
+        'clientSecret'  : '', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+};
